ec32eb218538ab49350bd5d79f2adeb931c22537
Charlie Keaney <charlie.keaney@embecosm.com>
2023-02-22 10:13:27 +0000

    Remove 'xcv' extension
    The CORE-V ISA Extension Naming specification was recently updated.
    One of the consequences of this is that the option previously
    called 'xcorev' (but implemented as 'xcv' presently due to the
    prefix change) has been removed. However, the compiler does not
    presently reflect this change. This patch removes the 'xcv' march
    flag option so that the compiler and the CORE-V ISA Extension Naming
    specification match.
    
    See here for CORE-V ISA Extension Naming specification:
    (https://github.com/openhwgroup/core-v-sw/blob/master/specifications/corev-isa-extension-naming.md)
    
    	* gcc/common/config/riscv/riscv-common.cc: Remove 'xcv' extension.
    	* gcc/config/riscv/riscv-opts.h: Likewise.

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -227,7 +227,6 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"svinval", ISA_SPEC_CLASS_NONE, 1, 0},
   {"svnapot", ISA_SPEC_CLASS_NONE, 1, 0},
 
-  {"xcv",    ISA_SPEC_CLASS_NONE, 1, 0},
   {"xcvelw", ISA_SPEC_CLASS_NONE, 1, 0},
   {"xcvmac", ISA_SPEC_CLASS_NONE, 1, 0},
   {"xcvbitmanip", ISA_SPEC_CLASS_NONE, 1, 0},
@@ -1259,7 +1258,6 @@ static const riscv_ext_flag_table_t riscv_ext_flag_table[] =
   {"svinval", &gcc_options::x_riscv_sv_subext, MASK_SVINVAL},
   {"svnapot", &gcc_options::x_riscv_sv_subext, MASK_SVNAPOT},
 
-  {"xcv",    &gcc_options::x_riscv_xcv_flags, MASK_XCV},
   {"xcvelw", &gcc_options::x_riscv_xcv_flags, MASK_XCVELW},
   {"xcvmac", &gcc_options::x_riscv_xcv_flags, MASK_XCVMAC},
   {"xcvbitmanip", &gcc_options::x_riscv_xcv_flags, MASK_XCVBITMANIP},
