90da773ba698ba417006a13e773eaec863da3151
Craig Blackmore <craig.blackmore@embecosm.com>
2023-06-14 17:57:56 +0100

    CORE-V: Fix typo in mac/msu tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/cv-march-xcvmac-test-autogeneration-mac.c:
    	Replace -02 with -O2.
    	* gcc.target/riscv/cv-march-xcvmac-test-autogeneration-msu.c:
    	Likewise.

--- gcc/testsuite/gcc.target/riscv/cv-march-xcvmac-test-autogeneration-msu.c
+++ gcc/testsuite/gcc.target/riscv/cv-march-xcvmac-test-autogeneration-msu.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-02 -march=rv32im_xcvmac1p0 -mabi=ilp32" } */
+/* { dg-options "-O2 -march=rv32im_xcvmac1p0 -mabi=ilp32" } */
 
 int foo(int a, int b, int c)
 {
